5cc772220689e3d64233d9fc7999ee890b23ea53
singhabhishek365 <singhabhishek2258643@gmail.com>
2024-12-31 00:00:49 +0530

    Built web
    Branch: main
    Commit: b935e08ad15f8c3da816b603780b85a62943ab4f
    
    package:peanut 5.1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bf06a67c3642171da1fb0890e80a3532",
+const RESOURCES = {"flutter_bootstrap.js": "f8f686282e33964581026a7c3aa60c31",
 "version.json": "0393b209219114e70a351574979470f4",
 "index.html": "5e87b933aea306391e54054aa0868a4d",
 "/": "5e87b933aea306391e54054aa0868a4d",
-"main.dart.js": "9d48c4405a69f68971aef306d025eda2",
+"main.dart.js": "de9224b4c6484af30defb729071d48c5",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
